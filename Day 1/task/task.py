136908481270827af65c03b545b6c435fdfbd52a
TrilochanprasadBH <tptrilochanprasad@gmail.com>
2025-01-03 22:23:04 +0530

    day1 completed!

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,9 +1,19 @@
-(print("Hello " + input("what is your name")))
+# username = input("input some string")
+# length = len(username)
+# print(length)
 
-"""hello
-this is nice 
-"""  #this is multi line comment  using triple """enter multi line comments here  """
+# print(len(input("put some text in this")))
 
+# rules for naming variables in python
 
-name = input("enter your name here, that will get stored in name variable")
-print(name,"printing name")
+# 1. cannot start with number
+# 2. use camel case or underscore
+# 3. cannot use special syntax texts like print, input etcetra as variable names
+
+print("welcome to my first day python micro project")
+city = input('enter the city you grew up in\n')  #adding \n sends the cursor to next line in terminal also ,
+# prints in new line when needed
+print(city)
+pet_name = input('what is name of your pet animal\n')
+print(pet_name)
+print( "your band name could be " + city + " " + pet_name)
