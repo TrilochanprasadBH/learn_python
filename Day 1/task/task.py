8696d2c90912df4e0ee56e2dbfc4af908dd5ebad
TrilochanprasadBH <tptrilochanprasad@gmail.com>
2025-01-03 20:15:53 +0530

    day1 learning

---
+++ Day 1/task/task.py
@@ -0,0 +1,9 @@
+(print("Hello " + input("what is your name")))
+
+"""hello
+this is nice 
+"""  #this is multi line comment  using triple """enter multi line comments here  """
+
+
+name = input("enter your name here, that will get stored in name variable")
+print(name,"printing name")
